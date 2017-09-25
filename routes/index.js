b191b6971effefe8de37b1791f28acc92f7ddfda
Thomas Nowak <tnowak@plex.com>
2017-09-25 15:23:59 -0400

    Tweak config

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var crypto = require('crypto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Landing' });
 });
 
 router.get('/login', function(req, res, next) {
