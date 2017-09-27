23009c94503860ad9d85f011ee894158fc960e4a
Thomas Nowak <tnowak@plex.com>
2017-09-27 19:11:31 -0400

    changed message

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Started listening on port 3000!')
 })
 
 module.exports = app;
